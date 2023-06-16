6a1801086597992e73b8b47daea0381973c0cb03
MsRapunzel <anastasya.zolotova11@gmail.com>
2023-06-16 22:18:55 +0300

    2. Common Library: - Database & Config Module -Abstract Repository - Reservations CRUD - Validation & Logging - Dockerize

---
+++ apps/reservations/src/main.ts
@@ -0,0 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { ReservationsModule } from './reservations.module';
+import { Logger } from 'nestjs-pino';
+
+async function bootstrap() {
+  console.log('Test');
+  const app = await NestFactory.create(ReservationsModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger));
+  await app.listen(3000);
+}
+bootstrap();

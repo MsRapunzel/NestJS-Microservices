e4dedb090eed4ace3105aabab2df40885d55918c
MsRapunzel <anastasya.zolotova11@gmail.com>
2023-06-18 12:52:14 +0300

    3. Authentication: - Users - Passport - Local Strategy - JWT Strategy - Common Auth Guard

---
+++ apps/auth/src/users/users.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { CurrentUser } from '@app/common';
+import { UserDocument } from './models/users.schema';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+
+@Controller('users')
+export class UsersController {
+     constructor(private readonly usersService: UsersService) { }
+
+     @Post()
+     async create(@Body() createUserDto: CreateUserDto) {
+          return this.usersService.create(createUserDto);
+     }
+
+     @Get()
+     @UseGuards(JwtAuthGuard)
+     async getUser(@CurrentUser() user: UserDocument) {
+          return user;
+     }
+}
--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,4 +1,4 @@
-import { Type } from "class-transformer";
+import { Type } from 'class-transformer';
 import { IsDate, IsNotEmpty, IsString } from "class-validator";
 
 export class CreateReservationDto {

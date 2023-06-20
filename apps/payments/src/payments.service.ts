808a00629658f1599c02b677c00a50433edc322e
MsRapunzel <anastasya.zolotova11@gmail.com>
2023-06-20 16:16:00 +0300

    5. Notifications - Emit Notifications - Email Notifications

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Stripe from 'stripe';
-import { CreateChargeDto } from '@app/common';
+import { NOTIFICATIONS_SERVICE } from '@app/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { PaymentsCreateChargeDto } from './dto/payments-create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
@@ -12,22 +14,23 @@ export class PaymentsService {
     },
   );
 
-  constructor(private readonly configService: ConfigService) { }
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(NOTIFICATIONS_SERVICE)
+    private readonly notificationService: ClientProxy,
+  ) { }
 
-  async createCharge({ amount, /*card*/ }: CreateChargeDto) {
-    /* const paymentMethod = await this.stripe.paymentMethods.create({
-      type: 'card',
-      card,
-    }); */
-
+  async createCharge({ amount, email, }: PaymentsCreateChargeDto) {
     const paymentIntent = await this.stripe.paymentIntents.create({
       amount: amount * 100,
       confirm: true,
       payment_method: 'pm_card_visa',
       currency: 'usd',
+    });
 
-      /* payment_method: paymentMethod.id, */
-      /* payment_method_types: ['card'], */
+    this.notificationService.emit('notify_email', {
+      email,
+      text: `Your payment of ${amount}$ has completed successfully.`,
     });
 
     return paymentIntent;
